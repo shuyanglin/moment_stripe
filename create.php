b58802bf4d3c5e06077d4aa2edd7ee1b3cc6397d
shuyanglin <shuyanglin.tw@gmail.com>
2020-05-04 17:10:51 +0100

    test payment

---
+++ create.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// This is your real test secret API key.
+\Stripe\Stripe::setApiKey('sk_test_W13HEPJUXJrbHeP1djU8Hsuq0074GxP9pr');
+
+function calculateOrderAmount(): int {
+	// Replace this constant with a calculation of the order's amount
+	// Calculate the order total on the server to prevent
+	// customers from directly manipulating the amount on the client
+	return 50;
+}
+
+header('Content-Type: application/json');
+
+try {
+	// retrieve JSON from POST body
+	$json_str = file_get_contents('php://input');
+	$json_obj = json_decode($json_str);
+
+	$paymentIntent = \Stripe\PaymentIntent::create([
+		'amount' => calculateOrderAmount(),
+		'currency' => 'gbp',
+	]);
+
+	$output = [
+		'clientSecret' => $paymentIntent->client_secret,
+	];
+
+	echo json_encode($output);
+} catch (Error $e) {
+	http_response_code(500);
+	echo json_encode(['error' => $e->getMessage()]);
+}
+?>
